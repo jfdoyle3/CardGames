7dbc25b245a3ecd9012a1e5fff4f5039cbccb306
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2021-10-13 07:05:57 -0400

    player can draw and play card from hand

--- src/com/company/uno_game/Table.java
+++ src/com/company/uno_game/Table.java
@@ -5,7 +5,7 @@ import com.company.console.Input;
 import com.company.deck.UnoDeck;
 
 public class Table {
-
+     private int card;
     public void playGame() {
         Hand hand=new Hand();
         UnoDeck deck = new UnoDeck();
@@ -17,10 +17,15 @@ public class Table {
             System.out.println(hand.displayHand());
             int min =0;
             int max=hand.getHandSize()-1;
-            int card = Input.getInt("pick a card "+min+" thru "+max, min, max, "enter a number.");
-
-            Card discard=hand.removeCard(card);
-            System.out.println("Card Chosen: "+discard.display());
+            System.out.println();
+            int menu = Input.getInt("1. play\n2.draw", 1 ,2,"enter a number.");
+            if(menu==1) {
+                card = Input.getInt("pick a card " + min + " thru " + max, min, max, "enter a number.");
+                Card discard = hand.removeCard(card);
+                System.out.println("Card Chosen: " + discard.display());
+            } else{
+                hand.addCard(deck.unoDraw());
+            }
             if (hand.getHandSize() == 0)
                 break;
         }
